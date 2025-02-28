c12fc0b0bf9be601eb563b9f8e5b0a4443c5106a
Kendi7 <01otienokennedy@gmail.com>
2025-02-28 17:49:42 +0300

    Trying working with branch

--- Today.py
+++ Today.py
@@ -1,3 +1,8 @@
 print("Hello Word")
 num1= float(input("Bonyeza numbari ya kwanza: "))
 num2= float(input("Bonyeza numbari ya pili: "))
+if num2 !=0:
+    Division=num1/num2
+else:
+    print("The number is not valid")
+    
